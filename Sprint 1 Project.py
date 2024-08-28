af15428c5c235c5d45e12235c07cc03c13d6455a
juliennekateponce <89396806+juliennekateponce@users.noreply.github.com>
2024-08-28 20:51:10 +0800

    Update Sprint 1 Project.py

--- Sprint 1 Project.py
+++ Sprint 1 Project.py
@@ -4,54 +4,74 @@ import pandas as pd
 st.title("Mapping MooLAH")
 st.write("Uncovering Spending Patterns and Customer Segments for Targeted Marketing")
 st.write("Group 5 - Sinigang")
+st.image("slides/1.jpg")
+st.image("slides/2.jpg")
 
 
-def introduction():
+def background():
 
     # Load photo
-    st.title("Introduction")
+    st.title("Data Background")
     # st.write("Provide a short description of the problem and your objective.")
-    st.image("")
+    st.image("slides/5.jpg")
+    st.image("slides/6.jpg")
+    st.image("slides/7.jpg")
+    st.image("slides/8.jpg")
+    st.image("slides/9.jpg")
+    st.image("slides/11.jpg")
 
-def fi_state_ph():
-    st.title("Methodology")
-    st.image("")
+def preprocessing():
+    st.title("Data Preprocessing")
+    st.image("slides/12.jpg")
+    st.image("slides/13.jpg")
+
+def eda():
+    st.title("EDA")
+    st.image("slides/14.jpg")
+    st.image("slides/15.jpg")
+    st.image("slides/16.jpg")
+    st.image("slides/17.jpg")
+
+def modeling():
+    st.title("Modeling")
+    st.image("slides/19.jpg")
+    st.image("slides/20.jpg")
+    st.image("slides/21.jpg")
+    st.image("slides/22.jpg")
+    st.image("slides/23.jpg")
+    st.image("slides/24.jpg")
+    st.image("slides/25.jpg")
+    st.image("slides/26.jpg")
+    st.image("slides/27.jpg")
+
+def findings():
+    st.title("Findings")
+    st.image("slides/29.jpg")
+    st.image("slides/30.jpg")
     
-def fi_state_worldwide():
-    st.title("Conclusions")
-    st.image("")
-
-def recommendations():
-    st.title("Recommendations")
-    st.image("")
-
-def the_team():
-    st.title("") 
-    st.image("")
-
 
 list_of_pages = [
-    "Introduction",
-    "Methodology",
-    "Conclusions",
-    "Recommendations",
-    ""
+    "Data Background",
+    "Data Preprocessing",
+    "EDA",
+    "Modeling",
+    "Findings"
 ]
 
 st.sidebar.title(':dollar: Mapping MooLAH')
 selection = st.sidebar.radio("Go to: ", list_of_pages)
 
-if selection == "Introduction":
-    introduction()
+if selection == "Data Background":
+    background()
 
-elif selection == "Methodology":
-    fi_state_ph()
+elif selection == "Data Preprocessing":
+    preprocessing()
 
-elif selection == "Conclusions":
-    fi_state_worldwide()
+elif selection == "EDA":
+    eda()
 
-elif selection == "Recommendations":
-    recommendations()
+elif selection == "Modeling":
+    modeling()
 
-elif selection == "":
-    the_team()
+elif selection == "Findings":
+    findings()
